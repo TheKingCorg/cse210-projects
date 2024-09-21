7bae9af4fcba49d9c8f806800609771e045b5864
Wyatt Corrigan <wyattcorrigan@gmail.com>
2024-09-21 14:02:02 -0600

    added Bond name generator

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        //Just a spacer here
+        Console.WriteLine();
+
+        //Gather first name
+        Console.Write("Enter your first name: ");
+        string fname = Console.ReadLine();
+        
+        //Gather last name
+        Console.Write("Enter your last name: ");
+        string lname = Console.ReadLine();
+        
+        //Display like Bond
+        Console.WriteLine();
+        Console.WriteLine($"Your name is {lname}, {fname} {lname}.");
     }
 }
