729ca4e7463c5f60fe42a1b929014eb029239d8d
Wyatt Corrigan <wyattcorrigan@gmail.com>
2024-09-21 17:51:28 -0600

    Built the number guesser program

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Opening spacers
+        Console.WriteLine();
+        Console.WriteLine();
+
+        //Computer picks a random number
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,101);
+        
+        //Start game
+        Console.WriteLine("I've picked a random number from 1 to 100! \nTo win, you have to guess it correctly!\n");
+        int userGuess = 0;
+        string rawInput = "";
+        int guessCount = 0;
+
+        //Game loop
+        do
+        {
+            Console.Write("Enter your guess here: ");
+            rawInput = Console.ReadLine();
+            userGuess = int.Parse(rawInput); //Converts user input into integer
+            guessCount ++;
+
+            //Determine and give hint
+            if (userGuess > magicNumber)
+            {
+                Console.WriteLine($"\n{rawInput} is too high! Try lower.");
+            }
+            else if (userGuess < magicNumber)
+            {
+                Console.WriteLine($"\n{rawInput} is too low! Try higher.");
+            }
+        } while (userGuess != magicNumber);
+
+        //Victory message
+        Console.WriteLine($"\nWow! You guessed it right in {guessCount} guesses! Good job!");
+        
+        //Opening spacers
+        Console.WriteLine();
+        Console.WriteLine();
     }
 }
