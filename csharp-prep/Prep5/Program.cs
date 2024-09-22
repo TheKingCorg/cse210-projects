583c22605ec55ff6b265a181e7848394be223470
Wyatt Corrigan <wyattcorrigan@gmail.com>
2024-09-21 21:20:40 -0600

    wrote the function practice program

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,53 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+
+        string userName = PromptUserName();
+
+        int favNum = PromptUserNumber();
+        
+        int theSquare = SquareNumber(favNum);
+
+        DisplayResult(userName, theSquare);
+    }
+
+    static void DisplayWelcome()
+    {
+        //spacers
+        Console.WriteLine();
+        Console.WriteLine();
+        //Displays the message, "Welcome to the Program!"
+        Console.WriteLine("\nWelcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        //Asks for and returns the user's name (as a string)
+        Console.Write("\nPlease enter your name here: ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+
+    static int PromptUserNumber ()
+    {
+        //Asks for and returns the user's favorite number (as an integer)
+        Console.Write("\nPlease enter your favorite number here: ");
+        string rawNum = Console.ReadLine();
+        int favNum = int.Parse(rawNum);
+        return favNum;
+    }
+
+    static int SquareNumber (int favNum)
+    {
+        //Accepts an integer as a parameter and returns that number squared (as an integer)
+        int favSquared = favNum * favNum;
+        return favSquared;
+    }
+
+    static void DisplayResult (string name, int square)
+    {
+        //Accepts the user's name and the squared number and displays them.
+        Console.WriteLine($"\nThank you {name}.\nYour favorite number squared is {square}.\n\n");
     }
 }
