9bb3b98b14a0588127f1bb15bcc23d1c17a07356
Wyatt Corrigan <wyattcorrigan@gmail.com>
2024-10-02 17:25:01 -0600

    early Fraction class setup

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,17 @@
+
+public class Fraction
+{
+    //Member variables
+    private string content = "";
+
+    //Constructor
+    public Fraction()
+    {
+    }
+
+    // A method that displays the person's job information
+    public void ResDisplay()
+    {
+
+    }
+}
