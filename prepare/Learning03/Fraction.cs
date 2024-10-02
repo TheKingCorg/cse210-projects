63246875ed3c5fdfa7df4f60109e6372a95ef26b
Wyatt Corrigan <wyattcorrigan@gmail.com>
2024-10-02 17:44:46 -0600

    flushed out Fraction constructors

--- prepare/Learning03/Fraction.cs
+++ prepare/Learning03/Fraction.cs
@@ -2,11 +2,27 @@
 public class Fraction
 {
     //Member variables
-    private string content = "";
+    private int _top;
+    private int _bottom;
 
-    //Constructor
+    //Constructors
     public Fraction()
     {
+        //for use without the user entering a value
+        _top = 1;
+        _bottom = 1;
+    }
+    public Fraction(int wholeNumber)
+    {
+        //For use when the user enters a single value
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+    public Fraction(int top, int bottom)
+    {
+        //for use when the user enters both top and bottom values
+        _top = top;
+        _bottom = bottom;
     }
 
     // A method that displays the person's job information
