b65ca605b02dbf1d40457ab746e6f6d65cfed8bc
Wyatt Corrigan <wyattcorrigan@gmail.com>
2024-10-03 16:00:14 -0600

    prototype of the HideRandomWords method in the Scripture class

--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -20,6 +20,21 @@ public class Scripture
     //Methods
     public void HideRandomWords(int numberToHide) //returns the value of a stored fraction's numerator
     {
+        Random random = new Random(); //random for use in hiding random words
+        int scriptLength = _words.Count; //The word count of whatever scripture is stored in the class
+        List<int> indexToHide = [];
+        for (int i = 0; i<numberToHide; i++)   //This loop will find 3 unhidden words to hide
+        {
+            int randIndex = random.Next(scriptLength + 1); //pick a random word from the list
+            if (_words[randIndex].IsHidden()) //check if it's hidden
+            { 
+                i -= 1; //if it is, decrement i so you'll loop through again
+            }
+            else
+            {
+                _words[randIndex].Hide();   //if not, hide the word
+            }
+        }
 
     }
     public string GetDisplayText()
