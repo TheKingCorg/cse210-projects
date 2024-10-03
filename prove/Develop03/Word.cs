f5188c5779636fca687c34cda21216d0c7d30257
Wyatt Corrigan <wyattcorrigan@gmail.com>
2024-10-03 15:19:22 -0600

    Finished Word class

--- prove/Develop03/Word.cs
+++ prove/Develop03/Word.cs
@@ -12,19 +12,28 @@ public class Word
     }
 
     //Methods
-    public void Hide() //returns the value of a stored fraction's numerator
+    public void Hide()
     {
+        _isHidden = true; //Turn a word hidden
     }
     public void Show()
     {
-
+        _isHidden = false; //unhide a hidden word
     }
     public bool IsHidden()
     {
-        return true;
+        return _isHidden; //return the current boolean value of the Word's _isHidden attribute
     }
     public string GetDisplayText()
     {
-        return "";
+        if (_isHidden)
+        {
+            int length = _text.Length;
+            return new string('_', length);
+        }
+        else
+        {
+            return ($"{_text}"); //return the text of the Word Class
+        }
     }
 }
