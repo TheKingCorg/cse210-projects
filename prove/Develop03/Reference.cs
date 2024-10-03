0afe8465a3e2767a6623d6aff653324a083c3a43
Wyatt Corrigan <wyattcorrigan@gmail.com>
2024-10-03 14:04:38 -0600

    Reference Class wireframe

--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -1,16 +1,29 @@
 public class Reference
 {
     //Member variables
-    private int _top;
+    private string _book;
+    private int _chapter;
+    private int _verse;
+    private int _endVerse;
 
     //Constructors
-    public Reference()
+    public Reference(string book, int chapter, int verse)
     {
+        _book = book;
+        _chapter = chapter;
+        _verse = verse;
+    }
+    public Reference(string book,int chapter,int startVerse, int endVerse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _verse = startVerse;
+        _endVerse = endVerse;
     }
 
     //Methods
-    public int GetTop() //returns the value of a stored fraction's numerator
+    public string GetDisplayText() //returns the value of a stored fraction's numerator
     {
-        return _top;
+        return "";
     }
 }
