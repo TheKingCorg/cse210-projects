fd5c16be7dd9862e97aee7bd5037dba77f4f6361
Wyatt Corrigan <wyattcorrigan@gmail.com>
2024-10-02 20:42:06 -0600

    Wrote program to test Fraction class

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction frac1 = new Fraction();
+        Console.WriteLine (frac1.GetDecimalValue());
+        Console.WriteLine (frac1.GetFractionString());
+
+        Fraction frac2 = new Fraction(3);
+        Console.WriteLine (frac2.GetDecimalValue());
+        Console.WriteLine (frac2.GetFractionString());
+
+        Fraction frac3 = new Fraction (5,20);
+        Console.WriteLine (frac3.GetDecimalValue());
+        Console.WriteLine (frac3.GetFractionString());
+
     }
 }
