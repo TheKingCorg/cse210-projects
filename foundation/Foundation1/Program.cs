5199914dce7921941613affbc9bfb4a8b973ea27
Wyatt Corrigan <wyattcorrigan@gmail.com>
2024-10-12 14:17:45 -0600

    Video class program 1.0

--- foundation/Foundation1/Program.cs
+++ foundation/Foundation1/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
+        //Clear console and set output settings
+        Console.Clear();
+        Console.BackgroundColor = ConsoleColor.White;
+        Console.ForegroundColor = ConsoleColor.Black;
+
         Console.WriteLine("Hello Foundation1 World!");
     }
 }
--- foundation/Foundation1/Video.cs
+++ foundation/Foundation1/Video.cs
@@ -9,6 +9,7 @@ public class Video
     //Constructors
     public Video(string title, string author, string length)
     {
+        //Instantiate the new Video using the passed in information
         _title = title;
         _author = author;
         _length = length;
@@ -18,18 +19,27 @@ public class Video
     //Methods
     public void ShowComments()
     {
-        
+        //Display each Comment in the comment list
+        foreach (Comment indComm in _comments)
+        {
+            Console.WriteLine($"{indComm.GetDisplayText()}\n");
+        }   
     }
-    public void AddComment()
+
+    public void AddComment(string name, string text)
     {
-    
+        //Create a new comment and add it to the video's comment list
+        Comment newComm = new Comment(name,text);
+        _comments.Add(newComm);
     }
+
     public void ShowVideoDetails()
     {
-    
+        Console.WriteLine($"{_title}\nBy {_author}\nLength: {_length}\nComments: {GetCommentNumber()}\n");
     }
     public int GetCommentNumber()
     {
-        return 0;
+        int commNum = _comments.Count();
+        return commNum;
     }
 }
