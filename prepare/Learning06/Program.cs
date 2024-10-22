7f404e367271948e5cb030bd26a1d55d916bc965
Wyatt Corrigan <wyattcorrigan@gmail.com>
2024-10-22 15:33:05 -0600

    corrected an error in Program.cs that made the shape list 3 of the same square

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -5,14 +5,14 @@ class Program
     static void Main(string[] args)
     {
         Console.Clear();
-        
+
         List<Shape> myShapes = new List<Shape>();
         Square mySquare = new Square("Red", 4);
         myShapes.Add(mySquare);
         Rectangle myRectangle = new Rectangle("Blue", 4, 5);
-        myShapes.Add(mySquare);
+        myShapes.Add(myRectangle);
         Circle myCircle = new Circle("Purple", 8);
-        myShapes.Add(mySquare);
+        myShapes.Add(myCircle);
 
         foreach (Shape shape in myShapes){
             Console.WriteLine($"\nColor: {shape.GetColor()}\nArea: {shape.GetArea()}");
