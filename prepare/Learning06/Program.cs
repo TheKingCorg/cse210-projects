998a1b86337ed5e5a2fda8578cd2719ac009d885
Wyatt Corrigan <wyattcorrigan@gmail.com>
2024-10-22 15:31:46 -0600

    Configured program

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+        Console.Clear();
+        
+        List<Shape> myShapes = new List<Shape>();
+        Square mySquare = new Square("Red", 4);
+        myShapes.Add(mySquare);
+        Rectangle myRectangle = new Rectangle("Blue", 4, 5);
+        myShapes.Add(mySquare);
+        Circle myCircle = new Circle("Purple", 8);
+        myShapes.Add(mySquare);
+
+        foreach (Shape shape in myShapes){
+            Console.WriteLine($"\nColor: {shape.GetColor()}\nArea: {shape.GetArea()}");
+        }
     }
 }
