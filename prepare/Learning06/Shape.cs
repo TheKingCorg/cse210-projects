71af8532717135fc0c70d5436b32fdb59d8fa368
Wyatt Corrigan <wyattcorrigan@gmail.com>
2024-10-22 14:43:24 -0600

    flushed out Shape.cs and its methods

--- prepare/Learning06/Shape.cs
+++ prepare/Learning06/Shape.cs
@@ -3,17 +3,27 @@ public abstract class Shape
     //Member variables
     private string _color;
 
+
     //Constructors
     public Shape(){
+        //handles no arguments
         _color = "undefined";
     }
+    public Shape(string setColor){
+        //handles color passed in as an argument
+        _color = setColor;
+    }
+
 
     //Methods
     public string GetColor(){
-        return "";
+        //returns the class's _color variable
+        return _color;
     }
     public void SetColor(string targColor){
+        _color = targColor;
     }
+
     //Abstract method
     public abstract double GetArea();
 }
