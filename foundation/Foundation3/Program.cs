f8fd723a2aca0edeef761402272f5a60c12c0405
Wyatt Corrigan <wyattcorrigan@gmail.com>
2024-10-29 23:29:44 -0600

    prototyped Program v1.0

--- foundation/Foundation3/Program.cs
+++ foundation/Foundation3/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation3 World!");
+        //List for storing activities
+        List<Activity> activities = [];
+
+        //Create running activity
+        activities.Add(new RunningActivity(DateTime.Now.Date, 12.6F, 1.8F));
+
+        //Create swimming activity
+        activities.Add(new SwimmingActivity(DateTime.Now.Date, 30.2F, 6));
+
+        //Create cycling activity
+        activities.Add(new CyclingActivity(DateTime.Now.Date, 26.7F, 9.8F));
+
+        //Clear and space console
+        Console.Clear();
+        Console.WriteLine("\n\n");
+        //Dispaly the activities
+        foreach (Activity activity in activities){
+            Console.WriteLine(activity.GetStringRepresentation());
+        }
     }
 }
